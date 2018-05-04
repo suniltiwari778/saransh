6340dc05fb34034554bf9e2a2f391d200200ef8a
Sunil <h.suniltiwari@gmail.com>
2018-05-04 23:03:29 +1000

    First commit

---
+++ src/main/java/com/saranshpower/SaranshApplication.java
@@ -0,0 +1,12 @@
+package com.saranshpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaranshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaranshApplication.class, args);
+	}
+}
